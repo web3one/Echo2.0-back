591bd3cd7ff61e06481478e061de6b74cfce2a52
acoki <gxzheng104@gmail.com>
2026-05-27 17:16:08 +0800

    fix: allow public market depth api

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/SaTokenConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/SaTokenConfig.java
@@ -34,6 +34,7 @@ public class SaTokenConfig implements WebMvcConfigurer {
                         "/api/user/backPwd",
                         "/api/user/bindPhoneEmail",
                         "/api/kline",
+                        "/api/market/depth",
                         "/api/newKline",
                         "/api/coin/list",
                         "/api/user/bindPhoneEmail",
